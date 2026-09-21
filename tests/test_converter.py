c35f3b69dbe3b224d7b6f614dd0802c3b4cd6900
TruongDayNe <lostpeanut123@gmail.com>
2026-09-21 15:59:44 +0700

    test: add unit tests for length conversion

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -3,7 +3,9 @@ import unittest
 from student_tools.converter import (
     celsius_to_fahrenheit,
     fahrenheit_to_celsius,
+    kilometers_to_meters,
     kilometers_to_miles,
+    meters_to_kilometers,
     miles_to_kilometers,
 )
 
@@ -18,6 +20,16 @@ class ConverterTests(unittest.TestCase):
     def test_distance_conversion_round_trip(self):
         miles = kilometers_to_miles(10)
         self.assertAlmostEqual(miles_to_kilometers(miles), 10)
+
+    def test_length_conversion(self):
+        self.assertEqual(meters_to_kilometers(1000), 1)
+        self.assertEqual(meters_to_kilometers(500), 0.5)
+        self.assertEqual(kilometers_to_meters(1), 1000)
+        self.assertEqual(kilometers_to_meters(0.5), 500)
+
+    def test_length_conversion_round_trip(self):
+        kilometers = meters_to_kilometers(2500)
+        self.assertAlmostEqual(kilometers_to_meters(kilometers), 2500)
 
 
 if __name__ == "__main__":
